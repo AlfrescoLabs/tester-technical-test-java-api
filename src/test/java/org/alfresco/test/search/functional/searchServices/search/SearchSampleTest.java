65c384ca971b7d62cb0a73a007cea593985d6890
meenal.bhave@alfresco.com <meenal.bhave@alfresco.com>
2019-07-19 17:45:51 +0100

    Initial Draft of technical test

---
+++ src/test/java/org/alfresco/test/search/functional/searchServices/search/SearchSampleTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019 Alfresco Software, Ltd. All rights reserved.
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
+package org.alfresco.test.search.functional.searchServices.search;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.rest.search.SearchResponse;
+import org.alfresco.test.search.functional.AbstractE2EFunctionalTest;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.data.DataContent;
+import org.alfresco.utility.data.DataSite;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.FileType;
+import org.alfresco.utility.model.FolderModel;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
+import org.apache.chemistry.opencmis.commons.enums.VersioningState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Purpose of this TestClass is to test that the search range query tests work as expected with CustomModels
+ * 
+ * @author meenal bhave
+ */
+
+public class SearchSampleTest extends AbstractE2EFunctionalTest
+{
+    @Autowired
+    protected DataSite dataSite;
+
+    @Autowired
+    protected DataContent dataContent;
+
+    private FileModel fileBanana, fileYellowBanana, fileBigYellowBanana, fileBigBananaBoat, fileYellowBananaBigBoat,
+            fileBigYellowBoat;
+
+    @BeforeClass(alwaysRun = true)
+    public void dataPreparation() throws Exception
+    {
+        serverHealth.assertServerIsOnline();
+
+        dataUser.addUserToSite(testUser, testSite, UserRole.SiteContributor);
+
+        // Create files with different phrases
+        fileBanana = new FileModel(unique_searchString + "-1.txt", "banana", "phrase searching", FileType.TEXT_PLAIN,
+                "banana");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(fileBanana);
+
+        fileYellowBanana = new FileModel(unique_searchString + "-2.txt", "yellow banana", "phrase searching",
+                FileType.TEXT_PLAIN, "yellow banana");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(fileYellowBanana);
+
+        fileBigYellowBanana = new FileModel(unique_searchString + "-3.txt", "big yellow banana", "phrase searching",
+                FileType.TEXT_PLAIN, "big yellow banana");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(fileBigYellowBanana);
+
+        fileBigBananaBoat = new FileModel(unique_searchString + "-4.txt", "big boat", "phrase searching",
+                FileType.TEXT_PLAIN, "big boat");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(fileBigBananaBoat);
+
+        fileYellowBananaBigBoat = new FileModel(unique_searchString + "-5.txt", "yellow banana big boat",
+                "phrase searching", FileType.TEXT_PLAIN, "yellow banana big boat");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(fileYellowBananaBigBoat);
+
+        fileBigYellowBoat = new FileModel(unique_searchString + "-6.txt", "big yellow boat", "phrase searching",
+                FileType.TEXT_PLAIN, "big yellow boat");
+        dataContent.usingUser(testUser).usingSite(testSite).createContent(fileBigYellowBoat);
+
+        waitForContentIndexing(fileBigYellowBoat.getName(), true);
+
+    }
+
+    @Test(priority = 1)
+    public void testSample()
+    {
+        // TODO: Implement a set of tests to test the Search API
+        Assert.assertEquals(1, 1);
+    }
+}
---
+++ src/test/java/org/alfresco/test/search/functional/searchServices/search/SetupTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 Alfresco Software, Ltd. All rights reserved.
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
+package org.alfresco.test.search.functional.searchServices.search;
+
+import org.alfresco.dataprep.SiteService.Visibility;
+import org.alfresco.rest.core.RestResponse;
+import org.alfresco.rest.search.SearchSqlJDBCRequest;
+import org.alfresco.rest.search.SearchSqlRequest;
+import org.alfresco.test.search.functional.AbstractE2EFunctionalTest;
+import org.alfresco.utility.constants.UserRole;
+import org.alfresco.utility.data.DataContent;
+import org.alfresco.utility.data.DataSite;
+import org.alfresco.utility.data.RandomData;
+import org.alfresco.utility.model.FileModel;
+import org.alfresco.utility.model.FileType;
+import org.alfresco.utility.model.FolderModel;
+import org.alfresco.utility.model.SiteModel;
+import org.alfresco.search.TestGroup;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
+import org.apache.chemistry.opencmis.commons.enums.VersioningState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Purpose of this Test is to test that the dependencies have been wired correctly and work well
+ * 
+ * @author meenal bhave
+ */
+public class SetupTest extends AbstractE2EFunctionalTest
+{
+    @Autowired
+    protected DataSite dataSite;
+
+    @Autowired
+    protected DataContent dataContent;
+
+    private FolderModel testFolder;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception
+    {
+        dataContent.usingSite(testSite).createFolder(FolderModel.getRandomFolderModel());
+
+        dataUser.addUserToSite(testUser, testSite, UserRole.SiteContributor);
+
+        testFolder = dataContent.usingSite(testSite).usingUser(testUser).createFolder();
+    }
+    
+    // Test Custom Model: Finance can be used
+    @Test(priority = 1)
+    public void testModelFinanceCanBeUsed() throws Exception
+    {
+        // Create document of custom type
+
+        FileModel customFile = FileModel.getRandomFileModel(FileType.TEXT_PLAIN, "searchContent-finance");
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(PropertyIds.OBJECT_TYPE_ID, "D:finance:Receipt");
+        properties.put(PropertyIds.NAME, customFile.getName());
+        properties.put("finance:ReceiptNo", 1);
+        properties.put("finance:ReceiptValue", 30);
+
+        cmisApi.authenticateUser(testUser).usingSite(testSite)
+                .usingResource(testFolder)
+                .createFile(customFile, properties, VersioningState.MAJOR)
+                .assertThat().existsInRepo();
+
+        Assert.assertTrue(waitForIndexing("cm:name:" + customFile.getName(),true),"New content could not be found");
+    }
+}
